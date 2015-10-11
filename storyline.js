a97ca717450835c0d1862aa24bdb59bef6fdae35
Simranpal Singh <navigill20@gmail.com>
2015-10-10 22:51:00 -0700

    getting user response

---
+++ storyline.js
@@ -0,0 +1,15 @@
+// Adding some story
+console.log("You are at a Justin Bieber concert, and you hear this lyric 'Lace my shoes off, start racing.'");
+
+//storyline
+console.log("Suddenly, Bieber stops and says, 'Who wants to race me?'");
+
+// Getting user's response
+var userAnswer = prompt("Do you want to race Bieber on stage?");
+
+if(userAnswer ==="yes") {
+console.log("You and Bieber start racing. It's neck and neck! You win by a shoelace!");
+}
+else {
+console.log("Oh no! Bieber shakes his head and sings 'I set a pace, so I can race without pacing.'");
+}
