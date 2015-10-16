0d326ba6a31a3dedb323ef3f1342fcff2ffdd503
Simranpal Singh <navigill20@gmail.com>
2015-10-15 20:26:15 -0700

    different array types in js

---
+++ arrays.js
@@ -0,0 +1,8 @@
+//heterogenous array
+var mix = ["a", true, 1];
+
+//multi-dimensional array
+var multi = [["A","B"], [1, 2], ['a','b']];
+
+//jagged array
+var jagged = [["A", true], [1, 2, 3], ['a','b']];
