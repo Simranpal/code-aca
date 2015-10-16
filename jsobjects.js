ec9011f87a672b25ecb2eb348edb2607df2e88e5
Simranpal Singh <navigill20@gmail.com>
2015-10-15 21:15:35 -0700

    declare objects in JS

---
+++ jsobjects.js
@@ -0,0 +1,19 @@
+var me = new Object();
+me.name = "simran";
+me["age"] = 24;
+
+//one way to create object
+var object1 = {
+ name : "simple",
+ class : "one"
+};
+
+//another way to create object
+var object2  = new Object();
+object2.name = "advanced";
+object2.type = "two";
+
+//another way to declare attributes
+var object3  = new Object();
+object3["name"] = "obj3";
+object3["category"] = "three";
