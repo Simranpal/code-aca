8cd4e43f021efd947a68e661ef3e6e14e7ce48b4
Simranpal Singh <navigill20@gmail.com>
2015-10-19 20:03:26 -0700

    created list of friends using nested objects

---
+++ contact-list-app/conact-list.js
@@ -0,0 +1,6 @@
+var friends = new Object();
+
+friends.john = { firstName : "John",lastName : "Doe", number : 1234 };
+friends.noah = { firstName : "Noah",lastName : "Gill", number : 25678 };
+friends.bill = { firstName : "Bill",lastName : "Young", number : 3891011 };
+friends.steve = {firstName : "Steve",lastName : "Jobs", number : 456574};
