f5c005862f2877edd021331c320d28b2f2b8be25
Simranpal Singh <navigill20@gmail.com>
2015-10-19 20:08:46 -0700

    added address key to all friends

--- contact-list-app/conact-list.js
+++ contact-list-app/conact-list.js
@@ -1,6 +1,26 @@
 var friends = new Object();
 
-friends.john = { firstName : "John",lastName : "Doe", number : 1234 };
-friends.noah = { firstName : "Noah",lastName : "Gill", number : 25678 };
-friends.bill = { firstName : "Bill",lastName : "Young", number : 3891011 };
-friends.steve = {firstName : "Steve",lastName : "Jobs", number : 456574};
+friends.john = { 
+    firstName : "John",
+    lastName : "Doe", 
+    number : 1234,
+    address: ['One Microsoft Way','Redmond','WA','98052']
+    };
+friends.noah = { 
+    firstName : "Noah",
+    lastName : "Gill", 
+    number : 25678, 
+    address: ['One Microsoft Way','Redmond','WA','98052']
+    };
+friends.bill = { 
+    firstName : "Bill",
+    lastName : "Young", 
+    number : 3891011, 
+    address: ['One Microsoft Way','Redmond','WA','98052']
+    };
+friends.steve = {
+    firstName : "Steve",
+    lastName : "Jobs", 
+    number : 456574,
+    address: ['One Microsoft Way','Redmond','WA','98052']
+    };
