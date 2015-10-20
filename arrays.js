84405b0d47b632c667aa7215407f499ee71cd3b1
Simranpal Singh <navigill20@gmail.com>
2015-10-19 19:39:20 -0700

    added objects to jagged array type

--- arrays.js
+++ arrays.js
@@ -9,3 +9,12 @@ var jagged = [["A", true], [1, 2, 3], ['a','b']];
 
 //heterogeneous array with object
 var myArray = [1, true, "simran",{}];
+
+//object
+var myObj = {
+    type: 'fancy',
+    disposition: 'sunny'
+};
+
+//jagged array with objects
+var newArray = [[1,2,3,4],[5,6,7],[{},myObj]];
