8be8e5ae91849d84b64a2e63cfc412c1dbf0be39
Simranpal Singh <navigill20@gmail.com>
2015-10-20 22:51:45 -0700

    updated the logic to print number and test for all conditions

--- fizbuzz.js
+++ fizbuzz.js
@@ -1,10 +1,12 @@
 var i;
 
 for(i = 1; i <= 20; i++ ){
-    if (i % 3 === 0)
+    if (i % 3 === 0 && i % 5 === 0)
+        console.log("FizzBuzz");
+    else if (i % 3 === 0)
         console.log("Fizz");
     else if (i % 5 === 0)
         console.log("Buzz");
     else
-        console.log("FizzBuzz");
+        console.log(i);
 }
